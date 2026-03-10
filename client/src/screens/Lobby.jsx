5a583def36c1a61869360c68ee917c065cac8805
shivrokks <126580397+shivrokks@users.noreply.github.com>
2026-03-10 17:30:41 +0530

    changes in lobby again

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -27,6 +27,7 @@ const LobbyScreen = () => {
 
   useEffect(() => {
     socket.on("room:join", handleJoinRoom);
+
     return () => {
       socket.off("room:join", handleJoinRoom);
     };
@@ -35,23 +36,30 @@ const LobbyScreen = () => {
   return (
     <div>
       <h1>Lobby</h1>
+
       <form onSubmit={handleSubmitForm}>
         <label htmlFor="email">Email ID</label>
+
         <input
           type="email"
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+
         <br />
+
         <label htmlFor="room">Room Number</label>
+
         <input
           type="text"
           id="room"
           value={room}
           onChange={(e) => setRoom(e.target.value)}
         />
+
         <br />
+
         <button>Join</button>
       </form>
     </div>
